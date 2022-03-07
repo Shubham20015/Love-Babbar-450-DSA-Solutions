16f88f6c0b22da73103f4801b8f59aed56b981e6
Shubham Jain <61053050+Shubham20015@users.noreply.github.com>
2022-03-08 00:24:35 +0530

    Add optimized code

--- heaps/MaxInKsubarray.java
+++ heaps/MaxInKsubarray.java
@@ -15,7 +15,7 @@ public class MaxInKsubarray {
 			arr[i] = sc.nextInt();
 		}
 		
-		int res[] = maxInSubarray(arr, n, k);
+		int res[] = maxSlidingWindow(arr, n, k);
 		
 		for(int val : res) {
 			System.out.print(val + " ");
@@ -24,29 +24,50 @@ public class MaxInKsubarray {
 		sc.close();
 	}
 	
-	static int[] maxInSubarray(int arr[],int n,int k) {
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+// 	TC: O(NlogN) , SC: O(logK)
+	static int[] maxSlidingWindow(int[] nums, int k) {
+        
+		int n = nums.length;
+		
 		int res[] = new int[n-k+1];
-		
-		if(k > n)
-			return res;
-		
-		for(int i=0;i<k;i++) {
-			pq.add(arr[i]);
-		}
-		
-		for(int i=k;i<n;i++) {
-			res[i-k] = pq.peek();
-			if(arr[i] > pq.peek()) {
-				pq.poll();
-				pq.add(arr[i]);
-			}
-		}
-		
-		res[n-k] = pq.poll();
-		
-		return res;
-	}
-	
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
 
+		for(int i=0;i<k;i++){
+		    pq.add(new int[] {nums[i],i});
+		}
+
+		res[0] = pq.peek()[0];
+		int index = 1;
+
+		for(int i=k;i<n;i++){
+		    pq.add(new int[] {nums[i],i});
+		    while(pq.peek()[1] <= i-k) pq.poll();
+		    res[index++] = pq.peek()[0];
+		}
+
+		return res;
+    }
+	
+// 	TC: O(N) , SC: O(K)
+	static int[] maxSlidingWindowOp(int[] nums, int k) {
+		int n = nums.length;
+		Deque<Integer> q = new ArrayDeque<>();
+		int res[] = new int[n-k+1];
+		int index = 0;
+
+		for(int i=0;i<n;i++){
+		    if(!q.isEmpty() && q.peek() == i-k)
+			q.poll();
+		    while(!q.isEmpty() && nums[q.peekLast()] < nums[i])
+			q.pollLast();
+
+		    q.offer(i);
+
+		    if(i >= k-1){
+			res[index++] = nums[q.peek()];
+		    }
+		}
+
+		return res;
+    }
 }
